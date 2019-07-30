524a0864e6c3fb8faf21ad2c4e503d73bceda3fb
zhouxuerong <1343932126@qq.com>
2019-07-30 19:58:04 +0800

    add routes

--- config/config.js
+++ config/config.js
@@ -5,7 +5,10 @@ export default {
             //umi-plugin-react插件配置 详情-》https://umijs.org/zh/plugin/umi-plugin-react.html#%E5%AE%89%E8%A3%85
             antd: true,
             dva: true,
-        }]
+            locale: {
+              enable: true,
+            },
+        }],
     ],
   routes: [{
     path: '/',
@@ -31,6 +34,12 @@ export default {
           { path: '/dashboard/workplace', component: 'Dashboard/Workplace' },
         ]
       },
-    ]
+    ],
   }],
+    proxy: {
+       '/dev': {
+         target: 'https://08ad1pao69.execute-api.us-east-1.amazonaws.com',
+         changeOrigin: true,
+       },
+      },
 };
